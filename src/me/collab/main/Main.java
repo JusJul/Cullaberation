4fe21cd8e2088902426c59dffb9531c2f0f48b58
ThoJulMic <thomasmichiels0@gmail.com>
2017-04-26 19:49:13 +0200

    dddddad

--- src/me/collab/main/Main.java
+++ src/me/collab/main/Main.java
@@ -1,8 +1,15 @@
 package me.collab.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener{
-
+public class Main extends JavaPlugin implements Listener {
+	
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	//Hello
+	
 }
