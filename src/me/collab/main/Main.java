823c01c3e25a4c25d1cbefd57724ae2c67494791
jdve99 <mezelf13@hotmail.com>
2017-04-26 19:55:40 +0200

    regeneration

--- src/me/collab/main/Main.java
+++ src/me/collab/main/Main.java
@@ -1,7 +1,12 @@
 package me.collab.main;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -12,8 +17,25 @@ public class Main extends JavaPlugin implements Listener {
 		Bukkit.getLogger().info("HAHA");
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
-	
-	//Hello
-	//homoo!
-	
+	@EventHandler
+	public void onEntityExplode(EntityExplodeEvent e) {
+		for (Block b : e.blockList()) {
+			final BlockState state = b.getState();
+			
+			b.setType(Material.AIR);
+			
+			int delay = 20;
+			
+			if((b.getType() == Material.SAND || (b.getType() == Material.GRAVEL))) {
+				delay += 1;
+			}
+			
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
+				public void run() {
+					state.update(true, false);
+				}
+			}, delay);
+			
+		}
+	}
 }
