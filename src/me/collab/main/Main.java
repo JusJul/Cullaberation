449f5b07e5dd0b65f3bdcd6f186233954cacd13d
jdve99 <mezelf13@hotmail.com>
2017-04-26 19:42:47 +0200

    rsgxfd

--- src/me/collab/main/Main.java
+++ src/me/collab/main/Main.java
@@ -1,7 +1,8 @@
 package me.collab.main;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin implements Listener{
 
 }
