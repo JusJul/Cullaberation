452c2a9ce432fbbf1b7f9a6278d521ec389bb8e9
jdve99 <mezelf13@hotmail.com>
2017-04-28 20:10:51 +0200

    first command

--- src/me/collab/main/Main.java
+++ src/me/collab/main/Main.java
@@ -1,26 +1,35 @@
 package me.collab.main;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-	
+
 	public void onEnable() {
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
-	
+
+	/*
+	 * 
+	 * 
+	 * 
+	 * HIER KAN JE UITLEG GOOIEN OVEWWR DINGEN
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("thomas")) {
+			if (sender.hasPermission("thomas.maak.usage"));			;
+
+		return true;
+
+	}
+		return true;
 }
-/*
- * 
- * 
- * 
- * 			HIER KAN JE UITLEG GOOIEN OVEWWR DINGEN
- * if (1 + 1 != 2) {
- * break.math!();
- * world.end
- * }
- * 
- * 
- * 
-*/
+}
